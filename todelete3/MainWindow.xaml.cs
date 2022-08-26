34170365f8d27991b6f65efb1078724e327561fe
Patrick Kranz <patrick-kranz@live.de>
2022-08-26 13:33:49 +0200

    …Add project files.

---
+++ todelete3/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace todelete3;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+[Mixin.Mixin(typeof(MyClass))]
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        textx.Text = NameTag;
+    }
+}
+
+internal class MyClass
+{
+    private const string NameTag = "Miximn";
+}
